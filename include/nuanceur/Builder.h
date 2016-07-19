cb90e226e19adfe2470cad85bc31826d477f87ad
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-07-19 10:12:24 -0400

    Add central header to use shader builder stuff in external projects.

---
+++ include/nuanceur/Builder.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "builder/ShaderBuilder.h"
+#include "builder/Float4Value.h"
+#include "builder/Texture2DValue.h"
+#include "builder/Matrix44Value.h"
+#include "builder/SwizzleSelector4.h"
+#include "builder/Operations.h"
