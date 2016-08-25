e4b85949aaeb033c4eff2a647bf372290f071eba
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-08-25 09:15:09 +0900

    Update Android project.

--- build_android/jni/Android.mk
+++ build_android/jni/Android.mk
@@ -1,16 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(LOCAL_PATH)/ExternalDependencies.mk
 FRAMEWORK_PATH := $(realpath $(LOCAL_PATH)/../../../Framework/)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE       := libNuanceur
-LOCAL_SRC_FILES    := ../../src/Operations.cpp \
-                      ../../src/GlslShaderGenerator.cpp \
-                      ../../src/ShaderBuilder.cpp \
-                      ../../src/SpirvShaderGenerator.cpp
-LOCAL_C_INCLUDES   := $(VULKAN_PATH) $(FRAMEWORK_PATH)/include $(LOCAL_PATH)/../../include
+LOCAL_SRC_FILES    := ../../src/builder/Operations.cpp \
+                      ../../src/builder/ShaderBuilder.cpp \
+                      ../../src/generators/GlslShaderGenerator.cpp \
+                      ../../src/generators/SpirvShaderGenerator.cpp
+LOCAL_C_INCLUDES   := $(VULKAN_SDK)/Include $(FRAMEWORK_PATH)/include $(LOCAL_PATH)/../../include
 LOCAL_CPP_FEATURES := exceptions rtti
 
 include $(BUILD_STATIC_LIBRARY)
